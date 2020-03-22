0f5eee3dfa402d8fa88baf03a0bfda6005ec8d39
wml <18270856781@163.com>
2020-03-22 22:57:28 +0800

    Signed-off-by: wml <18270856781@163.com>

---
+++ mybatisday1/src/edu/xidian/Test/Demo01.java
@@ -0,0 +1,19 @@
+package edu.xidian.Test;
+
+import org.junit.Test;
+
+public class Demo01 {
+    @Test
+    public void test1(){
+        //a)读取配置文件
+        //b)通过SqlSessionFactoryBuilder创建SqlSessionFactory会话工厂
+
+        new SqlSessionFactoryBuilder().build()
+        //c)通过SqlSessionFactory创建SqlSession
+
+        //d)调用SqlSession的操作数据库方法
+
+        //关闭SqlSession。
+    }
+
+}
