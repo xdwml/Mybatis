0f287193c35441a2e72e61b79926e6a31063308b
wml <18270856781@163.com>
2020-03-24 23:14:56 +0800

    Signed-off-by: wml <18270856781@163.com>

--- mybatisday1/src/edu/xidian/Test/Demo01.java
+++ mybatisday1/src/edu/xidian/Test/Demo01.java
@@ -8,7 +8,7 @@ public class Demo01 {
         //a)读取配置文件
         //b)通过SqlSessionFactoryBuilder创建SqlSessionFactory会话工厂
 
-        SqlSessionFactory
+        
         //c)通过SqlSessionFactory创建SqlSession
 
         //d)调用SqlSession的操作数据库方法
