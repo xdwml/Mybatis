5104ac491439323a2128a9b316894821559295e9
wml <18270856781@163.com>
2020-03-25 22:52:40 +0800

    Signed-off-by: wml <18270856781@163.com>

--- mybatisday1/src/edu/xidian/Test/Demo01.java
+++ mybatisday1/src/edu/xidian/Test/Demo01.java
@@ -2,13 +2,16 @@ package edu.xidian.Test;
 
 import org.junit.Test;
 
+import java.io.InputStream;
+
 public class Demo01 {
     @Test
     public void test1(){
         //a)读取配置文件
+        InputStream is=Resources.getRe;
         //b)通过SqlSessionFactoryBuilder创建SqlSessionFactory会话工厂
+        SqlSessionF
 
-        
         //c)通过SqlSessionFactory创建SqlSession
 
         //d)调用SqlSession的操作数据库方法
